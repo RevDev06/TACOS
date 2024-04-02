6f0892a22c4e757ff2ae0432ad03b469d107d6c9
Daniel Tenorio <69164086+RevDev06@users.noreply.github.com>
2024-04-01 21:05:21 -0600

    Se agrego la funcion getApachePort

--- detectorPuertos.py
+++ detectorPuertos.py
@@ -1,6 +1,6 @@
 import re
 
-def detectarPuerto(rutaXampp='C:/xampp/mysql/bin/my.ini'):
+def getXamppPort(rutaXampp='C:/xampp/mysql/bin/my.ini'):
     try:
         with open(rutaXampp, 'r') as archivo:
             contenido = archivo.read()
@@ -16,9 +16,30 @@ def detectarPuerto(rutaXampp='C:/xampp/mysql/bin/my.ini'):
         print(f"No se encontro Xampp en la ruta {rutaXampp}")
         return None
 
-# Uso de la función
-puerto = detectarPuerto()
-if puerto:
-    print(f"El puerto de mysql es {puerto}")
+def getApachePort(rutaApache='C:/xampp/apache/conf/httpd.conf'):
+    try:
+        with open(rutaApache, 'r') as archivo:
+            contenido = archivo.read()
+
+
+        resultados = re.findall(r'^Listen[ ]+(\d+)', contenido, re.MULTILINE)
+        if resultados:
+            return [int(puerto) for puerto in resultados]
+        else:
+            print("No se encontraron datos 'Listen' en el archivo")
+            return None
+    except FileNotFoundError:
+        print(f"No se pudo encontrar el archivo de configuración en {rutaApache}.")
+        return None
+
+puertoXampp = getXamppPort()
+puertoApache = getApachePort()
+if puertoXampp:
+    print(f"El puerto de mysql es {puertoXampp}")
+else:
+    print("No se pudo encontrar")
+
+if puertoApache:
+    print(f"El puerto de apache es {puertoApache}")
 else:
     print("No se pudo encontrar")
