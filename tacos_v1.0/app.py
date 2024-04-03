179da9cd51051452e6e0ec4ad159a970e6ada867
TenorioMartínezLeonardo <119890042+DowncastList1@users.noreply.github.com>
2024-04-03 07:46:36 -0600

    implementar("registro.html")

--- tacos_v1.0/app.py
+++ tacos_v1.0/app.py
@@ -2,13 +2,21 @@ from flask import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/login')
 def login():
-  return render_template("login.html")
+    return render_template("login.html")
+
+
+@app.route('/registro')
+def registro():
+    return render_template("registro.html")
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
