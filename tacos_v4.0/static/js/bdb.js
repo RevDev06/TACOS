404126991767db82b0254798d485558be409e386
Carlos M <123824839+carlosmax74@users.noreply.github.com>
2024-04-07 15:45:04 -0600

    Update bdb.js

--- tacos_v4.0/static/js/bdb.js
+++ tacos_v4.0/static/js/bdb.js
@@ -1,38 +1,44 @@
 const productos = [
-    { name: "Monitor" },
-    { name: "Mouse" },
-    { name: "Teclado" },
-    { name: "Tetas" },
-    { name: "Dildos para Oscar (de 70cm)" },
+    { name: "Monitor", description: "Descripción del monitor", image: "imagen_monitor.jpg" },
+    { name: "Mouse", description: "Descripción del mouse", image: "imagen_mouse.jpg" },
+    { name: "Teclado", description: "Descripción del teclado", image: "imagen_teclado.jpg" },
+    { name: "Tetas", description: "Descripción de las tetas", image: "imagen_tetas.jpg" },
+    { name: "Dildos para Oscar (de 70cm)", description: "Descripción del dildo", image: "imagen_dildo.jpg" },
 ];
 
 const searchInput = document.getElementById("searchInput");
-const resultList = document.getElementById("resultslist");
+const resultsContainer = document.getElementById("resultsContainer");
 const noResults = document.getElementById("noResults");
-
 
 const handleSearch = () => {
     const searchItem = searchInput.value.toLowerCase();
-    const filtreredproductos = productos.filter((producto) => producto.name.toLowerCase().startsWith
-        (searchItem));
+    const filteredProducts = productos.filter((producto) =>
+        producto.name.toLowerCase().includes(searchItem)
+    );
 
-    resultList.innerHTML = "";
+    resultsContainer.innerHTML = "";
 
-    if (filtreredproductos.length === 0) {
-        noResults.style.display = "block"
+    if (filteredProducts.length === 0) {
+        noResults.style.display = "block";
     } else {
-
-        filtreredproductos.forEach((producto) => {
-            const li = document.createElement("li");
-            li.textContent = producto.name;
-            resultList.appendChild(li);
+        filteredProducts.forEach((producto) => {
+            const card = `
+                <div class="card">
+                    <img src="${producto.image}" class="card-img-top" alt="${producto.name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${producto.name}</h5>
+                        <p class="card-text">${producto.description}</p>
+                    </div>
+                </div>
+            `;
+            resultsContainer.insertAdjacentHTML("beforeend", card);
         });
         noResults.style.display = "none";
-
     }
 
     if (searchInput.value === "") {
-        resultList.innerHTML = "";
+        resultsContainer.innerHTML = "";
+        noResults.style.display = "none";
     }
 };
 
